5a1468015192191e93a26e2789e43f676d01220d
ram.jeasta <ram.jeasta@avrioc.com>
2023-06-05 10:51:17 +0400

    Initial commit

---
+++ src/main/java/com/avrioc/utility/AvriocUtilityProjectSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.avrioc.utility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvriocUtilityProjectSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvriocUtilityProjectSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avrioc/utility/AvriocUtilityProjectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avrioc.utility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvriocUtilityProjectSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
